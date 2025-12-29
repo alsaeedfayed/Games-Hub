d575cf3fe481d3105a3027407db166186c021c74
alsaeedfayed <alsaeedfayed@gmail.com>
2025-12-29 14:54:00 -0500

    feat: :fire: Add swiper
    slides for games in hero section , split code => SwiperCards & CardInfo , add animation to pagination images using useState & useEffect by accessing the swiper instant

---
+++ app/components/CardInfo.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+const CardInfo = ({
+  desc,
+  title,
+  image,
+  textBtn,
+  btnClasses,
+}: {
+  desc: string;
+  title: string;
+  image: string;
+  textBtn?: string;
+  btnClasses?: string;
+}) => {
+  console.log(desc);
+  return (
+    <div>
+      <div className=" w-96 h-40 relative">
+        <Image src={image} fill alt={`${title}`} className=" object-contain" />
+      </div>
+      <h1 className=" text-white text-2xl font-semibold ">{title}</h1>
+      <p className=" text-base  text-gray-200">{desc}</p>
+      <Button className={`rounded-full mt-5 ${btnClasses || " text-gray-50"}`}>
+        {textBtn || "Find out more !"}
+      </Button>
+    </div>
+  );
+};
+
+export default CardInfo;
